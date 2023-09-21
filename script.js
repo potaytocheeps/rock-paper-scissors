0ea7c3312c4e64955f7f42e7e6a920354a4b3a97
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2023-09-20 22:03:12 -0700

    Add logic to play a round of Rock Paper Scissors
    Created the playRound() function which takes the player's selection
    and the computer's random selection and has the logic necessary to
    determine whether the player has won, lost, or tied the round.

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function getComputerChoice()
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    // Convert playerSelection to lowercase so that it will be case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+    let message = "";
+
+    // The switch statement will choose between the three conditions available:
+    // Player win, player loss, and a draw. This reduces the amount of chained
+    // and nested if-else statements that would be needed to achieve the same thing
+    switch (true)
+    {
+        // Player win conditions
+        case (playerSelection === "rock" && computerSelection === "scissors"):
+        case (playerSelection === "paper" && computerSelection === "rock"):
+        case (playerSelection === "scissors" && computerSelection === "paper"):
+            message = `You win! ${playerSelection} beats ${computerSelection}.`;
+            break;
+        // Player loss conditions
+        case (playerSelection === "rock" && computerSelection === "paper"):
+        case (playerSelection === "paper" && computerSelection === "scissors"):
+        case (playerSelection === "scissors" && computerSelection === "rock"):
+            message = `You lose! ${computerSelection} beats ${playerSelection}.`;
+            break;
+        // Draw condition
+        case (playerSelection === computerSelection):
+            message = `Draw! You both chose ${playerSelection}.`;
+            break;
+        default:
+            message = "Something went wrong!";
+    }
+
+    return message;
+}
